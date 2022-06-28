8123bb89b9065049cde346199d81342334c1ad04
ozanndemirell <ozandemirel.93@gmail.com>
2022-06-29 00:33:55 +0300

    Team names has been arranged. Arrangement for the last for is in next commit.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 import urllib.request
 import json
 from datetime import date
+import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -35,9 +36,13 @@ for match in matches:
             mtid = bet['MTID']
             if mtid == 1:
                 homeTeam = match['HN']
+                homeTeam = homeTeam.lower()
                 homeTeam = homeTeam.split(' ')
                 for x in homeTeam:
-                    print(homeTeam)
+                    if x == 'utd':
+                        x = 'United'
+                    elif x == 'atl':
+                        x = 'atletico'
                     if len(x) <= 2:
                         homeTeam.remove(x)
                         print(homeTeam)
@@ -53,8 +58,13 @@ for match in matches:
 
 
                 awayTeam = match['AN']
+                awayTeam = awayTeam.lower()
                 awayTeam = awayTeam.split(' ')
                 for y in awayTeam:
+                    if y == 'utd':
+                        y = 'united'
+                    elif y == 'atl':
+                        y = 'atletico'
                     if len(y) <= 2:
                         awayTeam.remove(y)
                         print(awayTeam)
@@ -65,11 +75,6 @@ for match in matches:
                 elif len(awayTeam) == 3:
                     awayTeam = awayTeam[0] + ' ' + awayTeam[1] + ' ' + awayTeam[2]
                 awayTeam = awayTeam.replace('-', ' ')
-                print(awayTeam)
-                print()
-
-
-
                 ratioH = ''
                 ratioD = ''
                 ratioA = ''
@@ -85,13 +90,9 @@ for match in matches:
                 nesineMatches.append(['Nesine', homeTeam, awayTeam, str(ratioH), str(ratioD), str(ratioA)])
 
 
-print(nesineMatches)
-print()
-
-
 tempoBetMatches = []
 driver = webdriver.Chrome()
-driver.get('https://www.686tempobet.com/todays_football.html')
+driver.get('https://www.687tempobet.com/todays_football.html')
 driver.maximize_window()
 driver.implicitly_wait(15)
 
@@ -112,7 +113,7 @@ for i in range(1, len(tempoBetLeagueList)+1):
         tempoBetMatchInfosList = driver.find_elements(By.XPATH, '//div[@class="collapsible"]/div[' + str(i) + ']/table/tbody/tr[' + str(j) + ']/td')
         for k in range(1, len(tempoBetMatchInfosList)):
             element = driver.find_element(By.XPATH, '//div[@class="collapsible"]/div[' + str(i) + ']/table/tbody/tr[' + str(j) + ']/td[' + str(k) + ']').text
-            if k==1:
+            if k == 1:
                 element = element.split('\n')
                 element = element[1].split(' - ')
                 print(element[0])
@@ -120,9 +121,14 @@ for i in range(1, len(tempoBetLeagueList)+1):
                 temporaryList.append('TempoBet')
                 for m in range(0, 2):
                     team = element[m]
+                    team = team.lower()
                     team = team.split(' ')
                     print(team)
                     for y in team:
+                        if team == 'utd':
+                            team = 'united'
+                        elif team == 'atl':
+                            team = 'atletico'
                         if len(y) <= 2:
                             team.remove(y)
                             print(team)
@@ -134,6 +140,8 @@ for i in range(1, len(tempoBetLeagueList)+1):
                         team = team[0] + ' ' + team[1] + ' ' + team[2]
                     elif len(team) == 4:
                         team = team[0] + ' ' + team[1] + ' ' + team[2] + '' + team[3]
+                    elif len(team) == 5:
+                        team = team[0] + ' ' + team[1] + ' ' + team[2] + '' + team[3] + '' + team[4]
                     print(team)
 
                     team = team.replace('-', ' ')
@@ -146,20 +154,22 @@ print(nesineMatches)
 print(tempoBetMatches)
 
 finalList = []
-df = pd.DataFrame(data=finalList, columns=['Home', 'Away', 'Nesine1', 'Nesine0', 'Nesine2','Tempo1','Tempo0','Tempo2'])
 
-
-print(len(nesineMatches))
-print(len(tempoBetMatches))
-for i in range(1, len(nesineMatches)+1):
-    print(nesineMatches[i][1],nesineMatches[i][2])
-    print()
-    for j in range(1, len(tempoBetMatches)+1):
-        print(i,j)
-        print(tempoBetMatches[j][1], tempoBetMatches[j][2])
-        print()
+for i in range(0, len(nesineMatches)):
+    for j in range(0, len(tempoBetMatches)):
         if nesineMatches[i][1] == tempoBetMatches[j][1] and nesineMatches[i][2] == tempoBetMatches[j][2]:
-            df.append(nesineMatches[i][1], nesineMatches[i][2], nesineMatches[3], nesineMatches[4], nesineMatches[5], tempoBetMatches[3], tempoBetMatches[4], tempoBetMatches[5])
+            print(nesineMatches[i][1])
+            print(nesineMatches[i][2])
+            print(nesineMatches[i][3])
+            print(nesineMatches[i][4])
+            print(nesineMatches[i][5])
+            print(tempoBetMatches[j][3])
+            print(tempoBetMatches[j][4])
+            print(tempoBetMatches[j][5])
+            finalList.append([nesineMatches[i][1], nesineMatches[i][2], nesineMatches[i][3], nesineMatches[i][4], nesineMatches[i][5], tempoBetMatches[j][3], tempoBetMatches[j][4], tempoBetMatches[j][5]])
+
+print(len(finalList))
+df = pd.DataFrame(data=finalList, columns=['Home', 'Away', 'Nesine1', 'Nesine0', 'Nesine2','Tempo1','Tempo0','Tempo2'])
 
 print(df)
 saveAsExcel(df)
